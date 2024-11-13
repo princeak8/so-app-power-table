371e0e9db47096a128451657903e6c0ce0000606
princeak <akalodave@gmail.com>
2024-11-13 23:04:01 +0100

    finished implementing download

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -18,6 +18,7 @@
                     <p>Start Date:<input type="date" v-model="startDate" :max="maxStartDate" /></p>
                     <p style="margin-left: 5%; margin-right: 2%;">End Date: <input type="date" v-model="endDate" :max="maxEndDate" :min="minEndDate" :disabled="!endDateActive" /></p>
                     <button type="button" @click="search"> Search </button>
+                    <button type="button" @click="download"> Download </button>
                     <button type="button" @click="clear"> Clear </button>           
                 </div>
                 
@@ -100,16 +101,65 @@
 
     const search = async () => {
         console.log('start date: ',startDate.value);
+        console.log('end date: ',endDate.value);
         if(startDate.value != undefined) {
             let url = `${import.meta.env.VITE_DB_URL}load_drop/range?start=${startDate.value}`;
             if(endDate.value != undefined) url += `&end=${endDate.value}`;
             await axios.get(url)
                 .then((res) => {
-                    // console.log('range: ',res.data);
-                    loadDrops.value = res.data;;
+                    console.log('range: ',res.data.data);
+                    loadDrops.value = res.data.data;
                 })
                 .catch((err) => {
                     console.log('Error:', err);
+                });
+        }
+    }
+
+    const download = async () => {
+        if(startDate.value != undefined) {
+            let url = `${import.meta.env.VITE_DB_URL}load_drop/download_range?start=${startDate.value}`;
+            if(endDate.value != undefined) url += `&end=${endDate.value}`;
+            await axios.get(url, {
+                        responseType: 'blob',
+                        headers: {
+                            'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                        }
+                    })
+                .then((res) => {
+                    console.log('Download Successful', res);
+                    // loadDrops.value = res.data;;
+
+                    // Create a Blob
+                    const blob = new Blob([res.data], { 
+                        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                    });
+
+                    // Get filename from response headers if available
+                    const contentDisposition = res.headers['content-disposition'];
+                    let filename = `loadDrop Report ${startDate.value}.xlsx`;
+                    if (contentDisposition) {
+                        const filenameMatch = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/);
+                        if (filenameMatch && filenameMatch[1]) {
+                            filename = filenameMatch[1].replace(/['"]/g, '');
+                        }
+                    }
+
+                    // Create download link
+                    const url = window.URL.createObjectURL(blob);
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.setAttribute('download', filename);
+                    document.body.appendChild(link);
+                    link.click();
+
+                    // Cleanup
+                    document.body.removeChild(link);
+                    window.URL.revokeObjectURL(url);
+
+                })
+                .catch((err) => {
+                    console.log('Download Error:', err);
                 });
         }
     }
