9aa8e49411a3591b496c21dd752565030182bfa7
Akachukwu Aneke <akalodave@gmail.com>
2025-09-13 02:49:24 +0100

    changes

--- src/stationComponents.ts
+++ src/stationComponents.ts
@@ -1,67 +1,67 @@
-import AfamIV from "@/components/AfamIV.vue";
-import AfamV from '@/components/AfamV.vue';
-import AfamVI from '@/components/AfamVI.vue';
-import Alaoji from '@/components/Alaoji.vue';
-import Azura from '@/components/Azura.vue';
-import Dadinkowa from '@/components/Dadinkowa.vue';
-import Delta2 from '@/components/Delta2.vue';
-import Delta3 from '@/components/Delta3.vue';
-import Delta4 from '@/components/Delta4.vue';
-import Egbin from '@/components/Egbin.vue';
-import Geregu from '@/components/Geregu.vue';
-import GereguNipp from '@/components/GereguNipp.vue';
-import Gbarain from '@/components/Gbarain.vue';
-import Ibom from '@/components/Ibom.vue';
-import Ihovbor from '@/components/Ihovbor.vue';
-import Jebba from '@/components/Jebba.vue';
-import Kainji from '@/components/Kainji.vue';
-import Odukpani from '@/components/Odukpani.vue';
-import Okpai from '@/components/Okpai.vue';
-import Olorunsogo from '@/components/Olorunsogo.vue';
-import OlorunsogoNipp from '@/components/OlorunsogoNipp.vue';
-import Omoku from '@/components/Omoku.vue';
-import Omotosho from '@/components/Omotosho.vue';
-import OmotoshoNipp from '@/components/OmotoshoNipp.vue';
-import ParasEnergy from '@/components/ParasEnergy.vue';
-import RiversIpp from '@/components/RiversIpp.vue';
-import SapeleNipp from '@/components/SapeleNipp.vue';
-import SapeleSteam from '@/components/SapeleSteam.vue';
-import Shiroro from '@/components/Shiroro.vue';
-import Taopex from '@/components/Taopex.vue';
-import Transamadi from '@/components/Transamadi.vue';
-import Zungeru from '@/components/Zungeru.vue';
+// import AfamIV from "@/components/AfamIV.vue";
+// import AfamV from '@/components/AfamV.vue';
+// import AfamVI from '@/components/AfamVI.vue';
+// import Alaoji from '@/components/Alaoji.vue';
+// import Azura from '@/components/Azura.vue';
+// import Dadinkowa from '@/components/Dadinkowa.vue';
+// import Delta2 from '@/components/Delta2.vue';
+// import Delta3 from '@/components/Delta3.vue';
+// import Delta4 from '@/components/Delta4.vue';
+// import Egbin from '@/components/Egbin.vue';
+// import Geregu from '@/components/Geregu.vue';
+// import GereguNipp from '@/components/GereguNipp.vue';
+// import Gbarain from '@/components/Gbarain.vue';
+// import Ibom from '@/components/Ibom.vue';
+// import Ihovbor from '@/components/Ihovbor.vue';
+// import Jebba from '@/components/Jebba.vue';
+// import Kainji from '@/components/Kainji.vue';
+// import Odukpani from '@/components/Odukpani.vue';
+// import Okpai from '@/components/Okpai.vue';
+// import Olorunsogo from '@/components/Olorunsogo.vue';
+// import OlorunsogoNipp from '@/components/OlorunsogoNipp.vue';
+// import Omoku from '@/components/Omoku.vue';
+// import Omotosho from '@/components/Omotosho.vue';
+// import OmotoshoNipp from '@/components/OmotoshoNipp.vue';
+// import ParasEnergy from '@/components/ParasEnergy.vue';
+// import RiversIpp from '@/components/RiversIpp.vue';
+// import SapeleNipp from '@/components/SapeleNipp.vue';
+// import SapeleSteam from '@/components/SapeleSteam.vue';
+// import Shiroro from '@/components/Shiroro.vue';
+// import Taopex from '@/components/Taopex.vue';
+// import Transamadi from '@/components/Transamadi.vue';
+// import Zungeru from '@/components/Zungeru.vue';
 
-export default [
-    AfamIV,
-    AfamV,
-    AfamVI,
-    Alaoji,
-    Azura,
-    Dadinkowa,
-    Delta2,
-    Delta3,
-    Delta4,
-    Egbin,
-    Geregu,
-    GereguNipp,
-    Gbarain,
-    Ibom,
-    Ihovbor,
-    Jebba,
-    Kainji,
-    Odukpani,
-    Okpai,
-    Olorunsogo,
-    OlorunsogoNipp,
-    Omoku,
-    Omotosho,
-    OmotoshoNipp,
-    ParasEnergy,
-    RiversIpp,
-    SapeleNipp,
-    SapeleSteam,
-    Shiroro,
-    Taopex,
-    Transamadi,
-    Zungeru
-]
+// export default [
+//     AfamIV,
+//     AfamV,
+//     AfamVI,
+//     Alaoji,
+//     Azura,
+//     Dadinkowa,
+//     Delta2,
+//     Delta3,
+//     Delta4,
+//     Egbin,
+//     Geregu,
+//     GereguNipp,
+//     Gbarain,
+//     Ibom,
+//     Ihovbor,
+//     Jebba,
+//     Kainji,
+//     Odukpani,
+//     Okpai,
+//     Olorunsogo,
+//     OlorunsogoNipp,
+//     Omoku,
+//     Omotosho,
+//     OmotoshoNipp,
+//     ParasEnergy,
+//     RiversIpp,
+//     SapeleNipp,
+//     SapeleSteam,
+//     Shiroro,
+//     Taopex,
+//     Transamadi,
+//     Zungeru
+// ]
